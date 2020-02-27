07fd0cdbccf4a62c46f07ad0a3c1ca13421d253d
Elias Norrby <elias.norrby@gmail.com>
2020-02-27 15:26:41 +0100

    L8: Lets implement the Service Layer (Business Logic)

---
+++ demo/src/main/java/com/eliasnorrby/demo/service/StudentService.java
@@ -0,0 +1,38 @@
+package com.eliasnorrby.demo.service;
+
+import com.eliasnorrby.demo.dao.StudentDao;
+import com.eliasnorrby.demo.model.Student;
+
+import java.util.List;
+import java.util.UUID;
+
+public class StudentService {
+
+  private final StudentDao studentDao;
+
+  public StudentService(StudentDao studentDao) {
+    this.studentDao = studentDao;
+  }
+
+  public int persistNewStudent(UUID studentId, Student student) {
+    UUID studentUid = studentId == null ? UUID.randomUUID() : studentId;
+    return studentDao.insertNewStudent(studentUid, student);
+  }
+
+  public Student getStudentById(UUID studentId) {
+    return studentDao.selectStudentById(studentId);
+  }
+
+  public List<Student> getAllStudents() {
+    return studentDao.selectAllStudents();
+  }
+
+  public int updateStudentById(UUID studentId, Student studentUpdate) {
+
+    return studentDao.updateStudentById(studentId, studentUpdate);
+  }
+
+  public int deleteStudentById(UUID studentId) {
+    return studentDao.deleteStudentById(studentId);
+  }
+}
